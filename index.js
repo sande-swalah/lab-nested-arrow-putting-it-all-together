9ef52abdad9df4a541e86da2fd14b3cabc28a367
sande-swalah <alvinswalah@gmail.com>
2025-11-28 20:06:00 +0300

    to be reviewed

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 
+//Define 
+function loginAttempt(userInfo){
+const userInfo= {
+  username: "user1"
+  password: "password123"
+}
+
+let attemptCount=0
+
+function userLogin() {
+  return (passwordAttempt,) => {
+  return attemptCount ++
+
+let attempt()
+
+  if (attemptCount > 3) {
+    return "Account locked due to too many failed log in attempts"
+  }
+
+  if (passwordAttempt === userInfo.password ) {
+    return "Login successful"
+
+   } else ( attemptCount < 3 ){ 
+   return "incorrect password.Try again"}
+}
+
+}
+
+userLogin()
+
+
+}
+
+ createLoginTracker()
 
 
 module.exports = {
